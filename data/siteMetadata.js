477d012288525993b1adbaad0a6e44c7c9f8b808
Ryan Griego <ryangriego@gmail.com>
2024-07-05 13:54:12 -0700

    adjusted nav links for live server

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -75,7 +75,7 @@ const siteMetadata = {
   search: {
     provider: 'kbar', // kbar or algolia
     kbarConfig: {
-      searchDocumentsPath: 'search.json', // path to load documents to search
+      searchDocumentsPath: 'blog/search.json', // path to load documents to search
     },
     // provider: 'algolia',
     // algoliaConfig: {
