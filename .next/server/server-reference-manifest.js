4e2f200993138d3cbb791cabec4508b1daea6f78
Ryan Griego <ryangriego@gmail.com>
2024-07-22 22:23:28 -0700

    Added next blog article and did some proofreading

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"JA3BJxe2yyKn8NGshMKF8kGvzdLaSONoMdn2q/+nAtU=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"GF5uNRly5zdJE9gW9LlDUbSJeAxVbJq98UxMgNotMJc=\"}"
