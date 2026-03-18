5828a298fcb349e35497f5542206034de780fb5c
Ryan Griego <ryangriego@gmail.com>
2026-03-17 18:27:41 -0700

    Remove .next from tracking and add to .gitignore

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Volumes/HomeX/ryangriegox/Desktop/projects/blog/app/layout":["static/media/36966cca54120369-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"k4lSdxIyW5oraz0ZI/jGLIDZnTenueeTmv3MzzkhQkc=\"}"
